29db164b20db5ae967d1b6053c95af6ddd15c59b
RohanBPatil <rohanpatil14218@gmail.com>
2020-10-15 12:50:47 +0530

    Added : Welcome message

---
+++ src/main/java/com/rohan/hotelmanagementsystem/HotelReservation.java
@@ -0,0 +1,7 @@
+package com.rohan.hotelmanagementsystem;
+
+public class HotelReservation {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Hotel Management Reservation Program");
+	}
+}
